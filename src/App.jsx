f39703e99748ec98d747161092695df2dfe596df
Dilip suthar <dilipkumarsuthar176@gmail.com>
2026-01-15 13:34:38 +0530

    Hero FIX IMG Change

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,16 @@ import About from "./pages/About";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 
-
 function App() {
   return (
     <>
       <Header />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<h1>Services</h1>} />
 <Route path="/work" element={<h1>Work</h1>} />
 <Route path="/contact" element={<h1>Contact</h1>} />
-
       </Routes>
    <Footer/>
     </>
